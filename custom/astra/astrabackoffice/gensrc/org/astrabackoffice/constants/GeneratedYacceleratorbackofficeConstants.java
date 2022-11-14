1315d8eaf4088b6b22a0cc0c5e0a611fc80f39a0
fajrulichsan <fajrulichsan0208@gmail.com>
2022-11-15 00:24:30 +0700

    moduleAstra

---
+++ custom/astra/astrabackoffice/gensrc/org/astrabackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14 Nov 2022 22.31.20                        ---
+ * ----------------------------------------------------------------
+ */
+package org.astrabackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "astrabackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ custom/astra/astrainitialdata/gensrc/org/astra/training/initialdata/constants/GeneratedAstraInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14 Nov 2022 22.31.20                        ---
+ * ----------------------------------------------------------------
+ */
+package org.astra.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedAstraInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "astrainitialdata";
+	
+	protected GeneratedAstraInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
